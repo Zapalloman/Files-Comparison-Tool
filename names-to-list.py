a66883caef0bb1b553328eaabda961b1670ba0cd
Javier Farías <64341277+Zapalloman@users.noreply.github.com>
2024-01-18 01:49:26 -0300

    Update names-to-list.py

--- names-to-list.py
+++ names-to-list.py
@@ -2,10 +2,10 @@ import os
 
 def obtener_lista_archivos(ruta_carpeta):
     try:
-        # Obtener la lista de archivos en la carpeta
+       
         archivos = os.listdir(ruta_carpeta)
 
-        # Filtrar solo los archivos (no directorios)
+        
         archivos = [archivo for archivo in archivos if os.path.isfile(os.path.join(ruta_carpeta, archivo))]
 
         return archivos
@@ -23,10 +23,10 @@ def guardar_lista_en_archivo(lista_archivos, nombre_archivo_salida):
         print(f"Error al guardar la lista en el archivo: {e}")
 
 if __name__ == "__main__":
-    # Ruta de la carpeta a analizar
+ #PUT YOUR PATH HERE
     ruta_carpeta = '(put your path here)'
 
-    # Obtener la lista de archivos
+
     lista_archivos = obtener_lista_archivos(ruta_carpeta)
 
     if lista_archivos:
